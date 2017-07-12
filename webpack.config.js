3702ad526623713bd70ddea8eaef5d8f40a9496e
Jesse Hoobergs <jesse@hoobergs.be>
2017-07-12 12:12:59 +0200

    Add basic configuration (webpack, eslint, babel, ...)

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack')
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
+const path = require('path')
+const env = require('yargs').argv.env
+
+let libraryName = 'rdocs-light'
+
+let plugins = []
+let outputFile
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }))
+  outputFile = libraryName + '.min.js'
+} else {
+  outputFile = libraryName + '.js'
+}
+
+const config = {
+  entry: path.join(__dirname, '/src/rdocs-light.js'),
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, '/lib'),
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        enforce: 'pre',
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
+  plugins: plugins
+}
+
+module.exports = config
